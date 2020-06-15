b74734467acecfe69410349a9f7400378b4edcd7
sadityaprdxn <adityas@prdxn.com>
2020-06-15 15:34:53 +0530

    index page javascript added for switching the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
-/* Author: 
+'use strict'
 
-*/
+window.onload = function() {
+
+	// FOR DOM MANIPULATION
+	var formSwitchingArray = this.Array.from(this.document.querySelectorAll('.pannel-container a'));
+
+	formSwitchingArray.forEach(function (element) {
+		
+		element.addEventListener('click' , function(){
+			var formContainer = document.querySelector('.form-container');
+	
+			if( element.id === "sign-in-button") {
+				formContainer.classList.add('active');
+				element.parentElement.classList.add('active');
+			} else if ( element.id === 'sign-up-button') {
+				formContainer.classList.remove('active');
+				element.parentElement.classList.remove('active');
+			}
+		});
+	});
+}
 
 
 
